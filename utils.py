d2a21493561c3e0cf6c7a9aad319c553d1eb9e49
edward <edwardinchina@outlook.com>
2022-06-16 02:28:02 +0000

    train on bucket
    add ability to store model and continue training on model

--- utils.py
+++ utils.py
@@ -66,3 +66,37 @@ def make_logdir(model_name: str):
     logdir = os.path.join(
         'runs', current_time + '_' + socket.gethostname() + '_' + model_name)
     return logdir
+
+
+
+
+from typing import Union
+from google.cloud.storage import Client
+
+def gcp_client():
+    credentials = 'gcp_config/gcp-service-account-file.json'
+    return Client.from_service_account_json(json_credentials_path=credentials)
+
+
+def upload_blob(bucket_name: str, source_file_name: str, destination_blob_name: str):
+    """Uploads a file to the google storage bucket.""" 
+    
+    storage_client = gcp_client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
+ 
+    blob.upload_from_filename(source_file_name)
+ 
+    print(f"{source_file_name} uploaded to Storage Bucket with blob name {destination_blob_name} successfully.")
+
+
+def download_blob(bucket_name: str, source_blob_name: str, destination_file_name: str):
+    """Downloads a blob."""
+ 
+    storage_client = gcp_client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(source_blob_name)
+    blob.download_to_filename(destination_file_name)
+ 
+    print(f"{source_blob_name} downloaded to file path {destination_file_name} successfully")
+
